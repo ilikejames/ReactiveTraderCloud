f864cbf0f3db2ea97e98c7e2dd65b227eae8fc4f
ilikejames <james.mountain@gmail.com>
2019-10-25 11:11:34 +0100

    wip 6

--- start-local-circleci.sh
+++ start-local-circleci.sh
@@ -5,5 +5,5 @@ CIRCLE_DOCKER_RUN_ARGUMENTS="-v /var/run/docker.sock:/var/run/docker.sock"  \
     --skip-checkout=false \
     --repo-url="/fake-remote" \
     --volume="/Users/james/code/ReactiveTraderCloud":"/fake-remote" \
-    --env DOCKER_USER=dockeruser \
+    --env DOCKER_USER=weareadaptive \
     --env TMP_BUILD_NUM=23
